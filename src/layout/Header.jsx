c1aeab8b6eccb6a24bd8f13fa4fb82e47503ca29
lemon <lemon@example.com>
2024-06-13 17:50:42 +0300

    commit 13.06_5

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="nav-wrapper">
         <a href="#" className="brand-logo">React Movies</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="https://github.com/lemon1964/react-movies.git">Repo</a></li>
+            <li><a href="#">Repo</a></li>
         </ul>
         </div>
     </nav>
