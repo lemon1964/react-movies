09d45accecf7613b00bade008079b24721f8db13
lemon <lemon@example.com>
2025-04-03 00:18:23 +0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,3 @@
-// import { Link } from 'react-router-dom';
-
-// function Header() {
-//     return (
-//         <nav className='green darken-1'>
-//             <div className='nav-wrapper'>
-//                 <Link to='/' className='brand-logo'>
-//                     Movies
-//                 </Link>
-//                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
-//                     <li>
-//                         <Link to={`/about`}>About</Link>
-//                     </li>
-//                     <li>
-//                         <Link to={`/contacts`}>Contacts</Link>
-//                     </li>
-//                 </ul>
-//             </div>
-//         </nav>
-//     );
-// }
-
-// export { Header };
 
 import { Link, useHistory } from 'react-router-dom';
 
@@ -44,17 +21,7 @@ function Header() {
                     </li>
                     <li>
                         <Link to={`/contacts`}>Contacts</Link>
-                    </li>
-                    <li>
-                        <a
-                            className="grey-text text-lighten-4 right"
-                            href="https://github.com/lemon1964/django-react-movies"
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            Repo
-                        </a>
-                    </li>                 
+                    </li>               
                 </ul>
             </div>
         </nav>
